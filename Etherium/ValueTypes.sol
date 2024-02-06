a121d0c5459dabece970b8dd7e031077919ce9d4
mdpamintuan <98503867+mdpamintuan@users.noreply.github.com>
2024-02-06 14:03:58 +0800

    Add files via upload

---
+++ Etherium/ValueTypes.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract ValueTypes {
+    bool public b = true;
+    uint public u = 123; // uint = uint256 0 to 2**256 - 1
+                        // uint8 0 to 2**8 - 1
+                        // uint = uint16 0 to 2**256 - 1
+    int public i = -123; // int = int256 -2**255 to 2**255 - 1
+                        // int128 -2**217 to 2**217 - 1
+    int public minInt = type(int).min;
+    int public maxInt = type(int).max;
+    address public addr =  0x1f9090aaE28b8a3dCeaDf281B0F12828e676c326;
+}
